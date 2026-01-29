be34b2aaf14430f336f31d7580cd4fea122cc366
Pratik Bugade <pratikbugade555@gmail.com>
2026-01-29 16:22:57 +0530

    created and used Template.json file

--- 4.Dynamic_prompt/Dynamic_prompt_ui.py
+++ 4.Dynamic_prompt/Dynamic_prompt_ui.py
@@ -17,26 +17,7 @@ length_input = st.selectbox("Select Explanation Length",["Short (1-2 paragraphs)
 
 #template
 
-template = PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following
-specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-- Include relevant mathematical equations if present in the paper.
-- Explain the mathematical concepts using simple, intuitive code snippets
-where applicable.
-2. Analogies:
-- Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient
-information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and
-length.
-""",
-
-input_variables=["paper_input","style_input","length_input"]
-)
+template = load_prompt("template.json")
 
 prompt = template.invoke({
     "paper_input" : paper_input,
