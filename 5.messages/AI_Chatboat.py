b0005fb1a9cd47c5b091d944b78eaea261d5557f
Pratik Bugade <pratikbugade555@gmail.com>
2026-01-29 17:04:12 +0530

    implement chatbot using LangChain messages (SystemMessage, HumanMessage, AIMessage)

---
+++ 5.messages/AI_Chatboat.py
@@ -0,0 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage,AIMessage,HumanMessage
+
+load_dotenv()
+
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+chat_history = [
+    SystemMessage(content="you are a helpful assistant")
+    ]
+
+while True:
+    
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+
+    if user_input == "exit":
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
+print(chat_history)
