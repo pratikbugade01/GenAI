15325f094705b3ce570a8fd61ee5bb7bebe13f3a
Pratik Bugade <pratikbugade555@gmail.com>
2026-02-16 19:12:09 +0530

    Runnable Lambda

--- 9.runnables/lambda_runnable.py
+++ 9.runnables/lambda_runnable.py
@@ -8,13 +8,13 @@ load_dotenv()
 
 model = GoogleGenerativeAI(model = "gemini-2.5-flash")
 
+def word_count(text):
+    return len(text.split())
+
 prompt1 = PromptTemplate(
     template='Write a joke about {topic}',
     input_variables=['topic']
 )
-
-def word_count(text):
-    return len(text.split())
 
 parser = StrOutputParser()
 
@@ -25,7 +25,7 @@ parallel_chain =  RunnableParallel({
     "word_count" : RunnableLambda(word_count)
 })
 
-final_chain = RunnableSequence(joke_gen_chain,parallel_chain)
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 
 result = final_chain.invoke({"topic":"AI"})
 
